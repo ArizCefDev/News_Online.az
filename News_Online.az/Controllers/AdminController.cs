cd5e08187a1a6e4ef9e9e37b06a3a53e75ccc60a
ArizCefDev <arizcefdev@gmail.com>
2023-12-23 22:12:34 +0400

    upt

---
+++ News_Online.az/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace News_Online.az.Controllers
+{
+    public class AdminController : Controller
+    {
+        AppDbContext db = new AppDbContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Category()
+        {
+            var values = db.Categories.ToList();
+            return View(values);
+        }
+
+        public IActionResult News()
+        {
+            var values = db.News.ToList();
+            return View(values);
+        }
+    }
+}
